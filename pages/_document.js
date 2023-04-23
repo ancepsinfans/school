b9361ec2d2a0bca97b113d26bc252ed294786518
ancepsinfans <zachary.r.bullard@gmail.com>
2023-04-23 11:12:34 +0300

    next attempt

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="nostr.json file" href="%PUBLIC_URL%/.well-known/nostr.json" />
+                    {/* <link rel="nostr file" href="%PUBLIC_URL%/.well-known/nostr" /> */}
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Vollkorn:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap" rel="stylesheet" />
