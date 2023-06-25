24c5f2148e7afafd86dc7eddc52b830e74efa1bc
ancepsinfans <zachary.r.bullard@gmail.com>
2023-06-25 09:42:47 +0300

    cleaned up posting in api routes

--- pages/api/user/postAnswer.js
+++ pages/api/user/postAnswer.js
@@ -1,42 +1,27 @@
+import ifDocExists from "../../../middleware/ifDocExists";
 import connectDB from "../../../middleware/mongodb";
 import { StudentAnswers, StudentSchema } from "../../../models/users/User";
+ifDocExists
 
 const handler = async (req, res) => {
   if (req.method === 'POST') {
     const { user, answer, correct, type, attempt, id, sphere, course, lesson } = req.body
-    if (user) {
-      try {
-        const quizAnswer = new StudentAnswers({
-          answer: answer,
-          correct: correct,
-          type: type,
-          attempt: attempt,
-          id: id,
-          sphere: sphere,
-          course: course,
-          lesson: lesson
-        })
 
-        let doc = await StudentSchema.findOneAndUpdate(
-          { user: user },
-          { upsert: true }
-        )
+    const quizAnswer = new StudentAnswers({
+      answer: answer,
+      correct: correct,
+      type: type,
+      attempt: attempt,
+      id: id,
+      sphere: sphere,
+      course: course,
+      lesson: lesson
+    })
 
-        if (!doc.answers) {
-          doc.answers = []
-        }
+    const result = ifDocExists(user, 'answers', quizAnswer, StudentSchema, res)
+    return result
 
-        doc.answers.push(quizAnswer)
 
-        doc.save()
-
-        return res.status(200).send(doc)
-      } catch (error) {
-        return res.status(500).send(error.message)
-      }
-    } else {
-      res.status(422).send('data_incomplete')
-    }
   } else {
     res.status(422).send('method not supported')
   }
