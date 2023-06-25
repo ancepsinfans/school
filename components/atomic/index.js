3cb1d4e906a89ee95122410a6de05f784e51ae28
ancepsinfans <zachary.r.bullard@gmail.com>
2023-06-25 07:33:46 +0300

    save?

--- components/atomic/index.js
+++ components/atomic/index.js
@@ -4,8 +4,8 @@ import SubmitButton from "./buttons/SubmitButton";
 import NextLessonButton from './buttons/NextLessonButton'
 import BlueButton from './buttons/BlueButton'
 import Popover from "./features/Popover";
-import TextInputQuiz from "./quiz/TextInputQuiz";
-import MCQuiz from "./quiz/MCQuiz/";
-import MCorOther from "./quiz/MCorOther";
+import TextInputQuiz from "./quizzes/TextInputQuiz";
+import MCQuiz from "./quizzes/MCQuiz/";
+import MCorOther from "./quizzes/MCorOther";
 
 export { SelectInput, TextInput, SubmitButton, NextLessonButton, BlueButton, Popover, MCQuiz, MCorOther, TextInputQuiz }
